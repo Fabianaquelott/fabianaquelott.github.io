fabc3a4769ee852e929b04154e54154c1f20b034
FabianaBulbe <fabiana.quelott@bulbe.com.br>
2026-03-05 17:14:10 -0300

    b

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portifolio-fabiana/",
+  base: "/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
